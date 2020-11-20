5809288361df6e1c97aa61537d75128c44268e87
Jeffrey Platt <jsplatt@gmail.com>
2020-11-20 17:10:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,12 +10,7 @@ end
 
 introduction_with_language("Jeff", "Ruby")
 
-def introduction_with_language_optional(name, language = "ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
-introduction_with_language("John")
-
+def introduction_with_language_optional
 
 
 
